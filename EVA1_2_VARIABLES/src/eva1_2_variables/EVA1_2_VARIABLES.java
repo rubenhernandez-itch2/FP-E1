69be244f26c1a0e23964c47c6fdac1ba81c2b90b
rubenhernandez-itch2 <81659471+rubenhernandez-itch2@users.noreply.github.com>
2024-08-23 17:54:05 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_variables;
+
+/**
+ *
+ * @author ruben
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                           //TIPOS DE DATOS
+        System.out.println("Hola mundo!!");//Cadena de texto
+        System.out.println(100);        //Entero
+        System.out.println(3.1416);     //Número con punto flotante
+        System.out.println('c');        //caracter
+        System.out.println(true);       //valores lógicos (booleanos)
+        System.out.println(
+                false
+        )
+                ;//; --> FIN DE INSTRUCCIÓN (NO SE LES OLVIDE PONERLO)
+        
+        //ENTEROS:
+        //int
+        //long
+        //short        
+        //NUMEROS CON PARTE DECIMAL (FLOTANTES):
+        //float
+        //double        
+        //CADENAS DE TEXTO:
+        //String        
+        //CARACTERES:
+        //char
+        //VALORES LÓGICOS:
+        //boolean
+        
+        //DECLARACIÓN DE VARIABLES:
+        //SINTAXIS PARA DECLARAR VARIABLES (REGLAS)
+        //TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE;
+        String nombre;
+        String apellido;
+        String curp;
+        float promedio;
+    }
+    
+}
